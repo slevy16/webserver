c805ca5b71626cda55eafb9564ba3345bc8d691b
Sarah Levy <slevy16@kentdenver.org>
2015-09-11 18:26:50 -0600

    add new pages

--- server.py
+++ server.py
@@ -2,14 +2,22 @@ import web
 render = web.template.render('templates/', base ='layout')
 
 urls = (
-    '/', 'index'
-)
+    '/', 'index',
+    '/feed', 'feed',
+    '/uploads', 'uploads'
+    )
+app = web.application(urls, globals(), True)
 
 class index:
     def GET(self):
         return render.index()
+class feed:
+    def GET(self):
+        return render.feed()
+class uploads:
+    def GET(self):
+        return render.uploads()
 
 #main method
 if __name__ == '__main__':
-    app = web.application(urls, globals())
     app.run()
