d8916aac3d59f84e55fced76ddda5efe9f5b5fcc
russell-stewart <rstewart16@kentdenver.org>
2015-09-13 18:26:56 -0600

    Add picture upload functionality

--- server.py
+++ server.py
@@ -18,6 +18,17 @@ class uploads:
     def GET(self):
         return render.uploads()
 
+    def POST(self):
+        x = web.input(myfile={})
+        filedir = 'static/uploads' # change this to the directory you want to store the file in.
+        if 'myfile' in x: # to check if the file-object is created
+            filepath=x.myfile.filename.replace('\\','/') # replaces the windows-style slashes with linux ones.
+            filename=filepath.split('/')[-1] # splits the and chooses the last part (the filename with extension)
+            fout = open(filedir +'/'+ filename,'w') # creates the file where the uploaded file should be stored
+            fout.write(x.myfile.file.read()) # writes the uploaded file to the newly created file.
+            fout.close() # closes the file, upload complete.
+        raise web.seeother('/feed')
+
 #main method
 if __name__ == '__main__':
     app.run()
